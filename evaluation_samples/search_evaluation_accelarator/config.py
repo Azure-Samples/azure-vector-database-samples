1ac87364259fd52860b6c8ce1c4cdff22a3d97ab
Raihan Alam <raihanalam@microsoft.com>
2024-05-09 11:07:07 +1000

    data loader

--- evaluation_samples/search_evaluation_accelarator/config.py
+++ evaluation_samples/search_evaluation_accelarator/config.py
@@ -9,6 +9,10 @@ class config():
     dataset_path: str
     embedding_dataset_path: str
     pre_chunked_dataset_path: str
+    ais_endpoint: str
+    ais_api_version: str
+    ais_key: str
+    index_name: str
 
     def read_config(self, config_file):
         config_file = os.path.join(os.getcwd(), 'evaluation_samples\search_evaluation_accelarator\config', config_file)
@@ -24,10 +28,14 @@ class config():
         self.aoai_api_version  = os.getenv("AOAI_API_VERSION")
         self.aoai_key  = os.getenv("AOAI_KEY")
         self.aoai_embedding_model  = os.getenv("AOAI_EMBEDDING_MODEL")
+        self.ais_endpoint  = os.getenv("AIS_ENDPOINT")
+        self.ais_api_version  = os.getenv("AIS_API_VERSION")
+        self.ais_key  = os.getenv("AIS_KEY")
 
         config_file = 'exp1_config.yml'
         exp_config = self.read_config(config_file)
         self.dataset_path = exp_config['embedding'][0]['dataset_path']
         self.embedding_dataset_path = exp_config['embedding'][1]['embedding_dataset_path']
         self.pre_chunked_dataset_path = exp_config['embedding'][2]['pre_chunked_dataset_path']
+        self.index_name = exp_config['data_load'][0]['index_name']
 
--- evaluation_samples/search_evaluation_accelarator/data_loader/ai_search.py
+++ evaluation_samples/search_evaluation_accelarator/data_loader/ai_search.py
@@ -2,12 +2,72 @@ from typing import Any
 from evaluation import evaluation_step
 import os
 from dotenv import load_dotenv
+import pandas as pd
 from azure.core.credentials import AzureKeyCredential
 from azure.search.documents import SearchClient
 from azure.search.documents.indexes import SearchIndexClient
+from azure.search.documents.indexes.models import (
+    SearchIndex,
+    SearchField,
+    SearchFieldDataType,
+    SimpleField,
+    SearchableField,
+    VectorSearch,
+    VectorSearchProfile,
+    HnswAlgorithmConfiguration
+)
+import time
 
 class aisearch_loader(evaluation_step):
-    def execute(self) -> str:
-        print("aisearch_loader")
-        print(self.config)
-        # super().handle_request(request)
+    def execute(self):
+        index_client = SearchIndexClient(self._config.ais_endpoint, AzureKeyCredential(self._config.ais_key))
+        index_client.delete_index(self._config.index_name)
+        time.sleep(5)
+
+        vector_search = VectorSearch(
+            profiles=[
+                VectorSearchProfile(
+                    name="hnsw_profile", algorithm_configuration_name="hnsw-algorithms-config"
+                )
+            ],
+            algorithms=[HnswAlgorithmConfiguration(name="hnsw-algorithms-config")],
+        )
+
+        fields = [
+            SimpleField(name="id", type=SearchFieldDataType.String, key=True),
+            SearchableField(name="chunk_content", type=SearchFieldDataType.String),
+            SearchField(name="chunk_content_vector", type=SearchFieldDataType.Collection(SearchFieldDataType.Single), searchable=True, vector_search_dimensions=1536, vector_search_profile_name="hnsw_profile"),
+            SearchableField(name="source_doc", type=SearchFieldDataType.String)   
+        ]
+
+        index = SearchIndex(
+            name=self._config.index_name,
+            fields=fields,
+            vector_search=vector_search
+            )
+        index_client = SearchIndexClient(self._config.ais_endpoint, AzureKeyCredential(self._config.ais_key))
+        index = index_client.create_index(index)
+    
+        doc_df = pd.read_json(self._config.embedding_dataset_path)
+
+        batch_size = 10
+        total_records = doc_df.shape[0]
+        fields = doc_df.columns.to_numpy()
+        doc_df['id'] = doc_df['id'].astype(str)
+
+        records = []
+
+        for index, row in doc_df.iterrows():
+            record = {}
+            for field in fields:
+                    record[field] = row[field]
+
+            records.append(
+                record
+            )
+
+            if index % batch_size == 0 or (index+1 == total_records):
+                search_client = SearchClient(self._config.ais_endpoint, self._config.index_name, AzureKeyCredential(self._config.ais_key))
+                result = search_client.upload_documents(documents=records)
+                records = []
+
