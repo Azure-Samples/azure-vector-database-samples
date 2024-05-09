588874c2b6f8b9ddc97293cce108cc32bd090d8a
Raihan Alam <raihanalam@microsoft.com>
2024-05-09 11:13:17 +1000

    data loader

--- evaluation_samples/search_evaluation_accelarator/data_loader/ai_search.py
+++ evaluation_samples/search_evaluation_accelarator/data_loader/ai_search.py
@@ -19,19 +19,25 @@ from azure.search.documents.indexes.models import (
 import time
 
 class aisearch_loader(evaluation_step):
+    def __init__(self, config):
+        super().__init__(config)
+
     def execute(self):
+        self.delete_existing_index()
+        time.sleep(5)  # Add a delay after index deletion, this needs to be replaced with an async call
+
+        index = self.create_search_index()
+        self.upload_documents_to_index(index)
+
+    def delete_existing_index(self):
         index_client = SearchIndexClient(self._config.ais_endpoint, AzureKeyCredential(self._config.ais_key))
         index_client.delete_index(self._config.index_name)
-        time.sleep(5)
 
-        vector_search = VectorSearch(
-            profiles=[
-                VectorSearchProfile(
-                    name="hnsw_profile", algorithm_configuration_name="hnsw-algorithms-config"
-                )
-            ],
-            algorithms=[HnswAlgorithmConfiguration(name="hnsw-algorithms-config")],
+    def create_search_index(self):
+        vector_search_profile = VectorSearchProfile(
+            name="hnsw_profile", algorithm_configuration_name="hnsw-algorithms-config"
         )
+        hnsw_algorithm_configuration = HnswAlgorithmConfiguration(name="hnsw-algorithms-config")
 
         fields = [
             SimpleField(name="id", type=SearchFieldDataType.String, key=True),
@@ -43,11 +49,13 @@ class aisearch_loader(evaluation_step):
         index = SearchIndex(
             name=self._config.index_name,
             fields=fields,
-            vector_search=vector_search
-            )
+            vector_search=VectorSearch(profiles=[vector_search_profile], algorithms=[hnsw_algorithm_configuration])
+        )
+
         index_client = SearchIndexClient(self._config.ais_endpoint, AzureKeyCredential(self._config.ais_key))
-        index = index_client.create_index(index)
-    
+        return index_client.create_index(index)
+
+    def upload_documents_to_index(self, index):
         doc_df = pd.read_json(self._config.embedding_dataset_path)
 
         batch_size = 10
@@ -60,14 +68,11 @@ class aisearch_loader(evaluation_step):
         for index, row in doc_df.iterrows():
             record = {}
             for field in fields:
-                    record[field] = row[field]
+                record[field] = row[field]
 
-            records.append(
-                record
-            )
+            records.append(record)
 
-            if index % batch_size == 0 or (index+1 == total_records):
+            if index % batch_size == 0 or (index + 1 == total_records):
                 search_client = SearchClient(self._config.ais_endpoint, self._config.index_name, AzureKeyCredential(self._config.ais_key))
-                result = search_client.upload_documents(documents=records)
+                search_client.upload_documents(documents=records)
                 records = []
-
