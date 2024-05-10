fa99cbc0af089b737fa4e9d132fd08c581e4e8ae
Raihan Alam <raihanalam@microsoft.com>
2024-05-10 10:11:13 +1000

    scorer

--- evaluation_samples/search_evaluation_accelarator/main.py
+++ evaluation_samples/search_evaluation_accelarator/main.py
@@ -9,8 +9,13 @@ if __name__ == "__main__":
     _config = config()
 
     eval_chain = evaluation_chain()
+    # generate embeddigns and save that as json file for the data that needs to go to the index
     eval_chain.add_evaluation_step(openai_embedding(_config))
+
+    # load the generated embeddings to ai search index
     eval_chain.add_evaluation_step(aisearch_loader(_config))
+
+    # use the eva dataset that we have to calcualte rougel score
     eval_chain.add_evaluation_step(rougel_evaluator(_config))
 
     eval_chain.execute()
